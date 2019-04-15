b6b84df431f3c65c5da8e871ce9e9800905147cc
EvolutionLT <33367201+EvolutionLT@users.noreply.github.com>
2019-04-15 23:32:22 +0800

    创建设计模式项目主工程

---
+++ src/main/java/com/designmode/DesignmodeApplication.java
@@ -0,0 +1,13 @@
+package com.designmode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignmodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignmodeApplication.class, args);
+	}
+
+}
